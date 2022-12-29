e6dd30ec1ac6262a9d06f18b28bd0229f831ea76
Shengzhong Liu <sl29@illinois.edu>
2022-12-29 14:31:25 -0600

    update test.py

--- src/test.py
+++ src/test.py
@@ -2,19 +2,20 @@ from numpy import argsort
 import torch.nn as nn
 
 # import models
-from models.MissSimulator import MissSimulator
+from data_augmenter.Augmenter import Augmenter
 from models.ResNet import ResNet
 from models.DeepSense import DeepSense
 from models.Transformer import Transformer
 from models.TransformerV2 import TransformerV2
 from models.TransformerV3 import TransformerV3
+from models.TransformerV4 import TransformerV4
 
 # utils
 from general_utils.time_utils import time_sync
 from general_utils.weight_utils import load_model_weight
 from params.test_params import parse_test_params
 from input_utils.multi_modal_dataloader import create_dataloader
-from train_utils.eval_functions import eval_given_model, eval_miss_detector
+from train_utils.eval_functions import eval_given_model
 
 
 def test(args):
@@ -23,29 +24,21 @@ def test(args):
     test_dataloader, _ = create_dataloader("test", args, batch_size=args.batch_size, workers=args.workers)
 
     # Init the miss modality simulator
-    miss_simulator = MissSimulator(args)
-    miss_simulator.to(args.device)
-    args.miss_simulator = miss_simulator
-
-    # Load the miss handler
-    if miss_simulator.miss_detector.trainable and args.detector_weight is not None:
-        miss_simulator.miss_detector = load_model_weight(miss_simulator.miss_detector, args.detector_weight)
-
-    # Load the miss detector
-    if miss_simulator.miss_handler.trainable and args.handler_weight is not None:
-        miss_simulator.miss_handler = load_model_weight(miss_simulator.miss_handler, args.handler_weight)
+    augmenter = Augmenter(args)
+    augmenter.to(args.device)
+    args.augmenter = augmenter
 
     # Init the classifier model
     if args.model == "DeepSense":
         classifier = DeepSense(args, self_attention=False)
-    elif args.model == "SADeepSense":
-        classifier = DeepSense(args, self_attention=True)
     elif args.model == "Transformer":
         classifier = Transformer(args)
     elif args.model == "TransformerV2":
         classifier = TransformerV2(args)
     elif args.model == "TransformerV3":
         classifier = TransformerV3(args)
+    elif args.model == "TransformerV4":
+        classifier = TransformerV4(args)
     elif args.model == "ResNet":
         classifier = ResNet(args)
     else:
@@ -66,21 +59,12 @@ def test(args):
             if param.requires_grad:
                 print(name)
 
-    # print resilient handler parameters
-    if "ResilientHandler" in args.miss_handler:
-        print("----------------------Resilient Handler Parameters----------------------")
-        for name, param in miss_simulator.miss_handler.named_parameters():
-            print(f"{name} \n", param.data.cpu().numpy())
-        print("------------------------------------------------------------------------")
-
-    test_classifier_loss, test_handler_loss, test_f1, test_acc, test_conf_matrix = eval_given_model(
-        args, classifier, miss_simulator, test_dataloader, classifier_loss_func
+    test_classifier_loss, test_f1, test_acc, test_conf_matrix = eval_given_model(
+        args, classifier, augmenter, test_dataloader, classifier_loss_func
     )
-    print(f"Test classifier loss: {test_classifier_loss: .5f}, test handler loss: {test_handler_loss: .5f}")
+    print(f"Test classifier loss: {test_classifier_loss: .5f}")
     print(f"Test acc: {test_acc: .5f}, test f1: {test_f1: .5f}")
     print(f"Test confusion matrix:\n {test_conf_matrix}")
-    if args.eval_detector:
-        eval_miss_detector(args, "Test", miss_simulator, eval_roc=True)
 
 
 def main_test():
