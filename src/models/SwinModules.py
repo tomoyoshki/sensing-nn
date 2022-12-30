b024f48f2b814088adce2ee5255f304ee8a92e41
Shengzhong Liu <sl29@illinois.edu>
2022-12-30 13:11:54 -0600

    revert fusion in TransformerV4 to Transformer block

--- src/models/SwinModules.py
+++ src/models/SwinModules.py
@@ -1,4 +1,4 @@
-'''
+"""
 @ref: https://github.com/microsoft/Swin-Transformer/blob/main/models/swin_transformer.py
 @inproceedings{liu2021Swin,
   title={Swin Transformer: Hierarchical Vision Transformer using Shifted Windows},
@@ -6,14 +6,15 @@
   booktitle={Proceedings of the IEEE/CVF International Conference on Computer Vision (ICCV)},
   year={2021}
 }
-'''
+"""
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
+
 class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.0):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
@@ -29,6 +30,7 @@ class Mlp(nn.Module):
         x = self.fc2(x)
         x = self.drop(x)
         return x
+
 
 def window_partition(x, window_size):
     """
@@ -62,7 +64,7 @@ def window_reverse(windows, window_size, H, W):
 
 
 class WindowAttention(nn.Module):
-    r'''
+    r"""
     Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window.
     Args:
@@ -73,20 +75,21 @@ class WindowAttention(nn.Module):
         qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
         attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
         proj_drop (float, optional): Dropout ratio of output. Default: 0.0
-    '''
+    """
 
-    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0.0, proj_drop=0.0):
 
         super().__init__()
         self.dim = dim
         self.window_size = window_size  # Wh, Ww
         self.num_heads = num_heads
         head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.scale = qk_scale or head_dim**-0.5
 
         # define a parameter table of relative position bias
         self.relative_position_bias_table = nn.Parameter(
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads)
+        )  # 2*Wh-1 * 2*Ww-1, nH
 
         # get pair-wise relative position index for each token inside the window
         coords_h = torch.arange(self.window_size[0])
@@ -106,23 +109,24 @@ class WindowAttention(nn.Module):
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
 
-        trunc_normal_(self.relative_position_bias_table, std=.02)
+        trunc_normal_(self.relative_position_bias_table, std=0.02)
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x, mask=None):
-        '''
+        """
         Args:
             x: input features with shape of (num_windows*B, N, C)
             mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
-        '''
+        """
         B_, N, C = x.shape
         qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
         q = q * self.scale
-        attn = (q @ k.transpose(-2, -1))
+        attn = q @ k.transpose(-2, -1)
 
         relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1
+        )  # Wh*Ww,Wh*Ww,nH
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
         attn = attn + relative_position_bias.unsqueeze(0)
 
@@ -142,7 +146,7 @@ class WindowAttention(nn.Module):
         return x
 
     def extra_repr(self) -> str:
-        return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
+        return f"dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}"
 
     def flops(self, N):
         # calculate flops for 1 window with token length of N
@@ -159,7 +163,7 @@ class WindowAttention(nn.Module):
 
 
 class SwinTransformerBlock(nn.Module):
-    '''
+    """
     Swin Transformer Block.
     Args:
         dim (int): Number of input channels.
@@ -176,12 +180,25 @@ class SwinTransformerBlock(nn.Module):
         act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
         norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
         fused_window_process (bool, optional): If True, use one kernel to fused window shift & window partition for acceleration, similar for the reversed part. Default: False
-    ''' 
+    """
 
-    def __init__(self, dim, input_resolution, num_heads, window_size=7, shift_size=0,
-                 mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0., drop_path=0.,
-                 act_layer=nn.GELU, norm_layer=nn.LayerNorm,
-                 fused_window_process=False):
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        num_heads,
+        window_size=7,
+        shift_size=0,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+        fused_window_process=False,
+    ):
         super().__init__()
         self.dim = dim
         self.input_resolution = input_resolution
@@ -193,14 +210,22 @@ class SwinTransformerBlock(nn.Module):
             # if window size is larger than input resolution, we don't partition windows
             self.shift_size = 0
             self.window_size = min(self.input_resolution)
-        assert 0 <= self.shift_size < self.window_size, f"shift_size must in 0-window_size: {self.shift_size} - {self.window_size}"
+        assert (
+            0 <= self.shift_size < self.window_size
+        ), f"shift_size must in 0-window_size: {self.shift_size} - {self.window_size}"
 
         self.norm1 = norm_layer(dim)
         self.attn = WindowAttention(
-            dim, window_size=to_2tuple(self.window_size), num_heads=num_heads,
-            qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+            dim,
+            window_size=to_2tuple(self.window_size),
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
 
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
@@ -209,12 +234,16 @@ class SwinTransformerBlock(nn.Module):
             # calculate attention mask for SW-MSA
             H, W = self.input_resolution
             img_mask = torch.zeros((1, H, W, 1))  # 1 H W 1
-            h_slices = (slice(0, -self.window_size),
-                        slice(-self.window_size, -self.shift_size),
-                        slice(-self.shift_size, None))
-            w_slices = (slice(0, -self.window_size),
-                        slice(-self.window_size, -self.shift_size),
-                        slice(-self.shift_size, None))
+            h_slices = (
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.shift_size),
+                slice(-self.shift_size, None),
+            )
+            w_slices = (
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.shift_size),
+                slice(-self.shift_size, None),
+            )
             cnt = 0
             for h in h_slices:
                 for w in w_slices:
@@ -283,8 +312,10 @@ class SwinTransformerBlock(nn.Module):
         return x
 
     def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
-               f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
+        return (
+            f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, "
+            f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
+        )
 
     def flops(self):
         flops = 0
@@ -302,12 +333,12 @@ class SwinTransformerBlock(nn.Module):
 
 
 class PatchMerging(nn.Module):
-    ''' Patch Merging Layer.
+    """Patch Merging Layer.
     Args:
         input_resolution (tuple[int]): Resolution of input feature.
         dim (int): Number of input channels.
         norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
-    '''
+    """
 
     def __init__(self, input_resolution, dim, norm_layer=nn.LayerNorm):
         super().__init__()
@@ -350,7 +381,7 @@ class PatchMerging(nn.Module):
 
 
 class BasicLayer(nn.Module):
-    '''
+    """
     A basic Swin Transformer layer for one stage.
     Args:
         dim (int): Number of input channels.
@@ -368,12 +399,26 @@ class BasicLayer(nn.Module):
         downsample (nn.Module | None, optional): Downsample layer at the end of the layer. Default: None
         use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False.
         fused_window_process (bool, optional): If True, use one kernel to fused window shift & window partition for acceleration, similar for the reversed part. Default: False
-    '''
+    """
 
-    def __init__(self, dim, input_resolution, depth, num_heads, window_size,
-                 mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., norm_layer=nn.LayerNorm, downsample=None, use_checkpoint=False,
-                 fused_window_process=False):
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        depth,
+        num_heads,
+        window_size,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        norm_layer=nn.LayerNorm,
+        downsample=None,
+        use_checkpoint=False,
+        fused_window_process=False,
+    ):
 
         super().__init__()
         self.dim = dim
@@ -382,17 +427,26 @@ class BasicLayer(nn.Module):
         self.use_checkpoint = use_checkpoint
 
         # build blocks
-        self.blocks = nn.ModuleList([
-            SwinTransformerBlock(dim=dim, input_resolution=input_resolution,
-                                 num_heads=num_heads, window_size=window_size,
-                                 shift_size=0 if (i % 2 == 0) else window_size // 2,
-                                 mlp_ratio=mlp_ratio,
-                                 qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                 drop=drop, attn_drop=attn_drop,
-                                 drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
-                                 norm_layer=norm_layer,
-                                 fused_window_process=fused_window_process)
-            for i in range(depth)])
+        self.blocks = nn.ModuleList(
+            [
+                SwinTransformerBlock(
+                    dim=dim,
+                    input_resolution=input_resolution,
+                    num_heads=num_heads,
+                    window_size=window_size,
+                    shift_size=0 if (i % 2 == 0) else window_size // 2,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop,
+                    attn_drop=attn_drop,
+                    drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
+                    norm_layer=norm_layer,
+                    fused_window_process=fused_window_process,
+                )
+                for i in range(depth)
+            ]
+        )
 
         # patch merging layer
         if downsample is not None:
@@ -423,14 +477,14 @@ class BasicLayer(nn.Module):
 
 
 class PatchEmbed(nn.Module):
-    ''' Image to Patch Embedding
+    """Image to Patch Embedding
     Args:
         img_size (int): Image size.  Default: 224.
         patch_size (int): Patch token size. Default: 4.
         in_chans (int): Number of input image channels. Default: 3.
         embed_dim (int): Number of linear projection output channels. Default: 96.
         norm_layer (nn.Module, optional): Normalization layer. Default: None
-    '''
+    """
 
     def __init__(self, img_size=(224, 224), patch_size=4, in_chans=3, embed_dim=96, norm_layer=None, stride=1):
         super().__init__()
@@ -454,8 +508,9 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         B, C, H, W = x.shape
         # FIXME look at relaxing size constraints
-        assert H == self.img_size[0] and W == self.img_size[1], \
-            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        assert (
+            H == self.img_size[0] and W == self.img_size[1]
+        ), f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
 
         x = self.proj(x)  # B Ph*Pw C
         x = x.flatten(2).transpose(1, 2)
--- src/models/TransformerV4.py
+++ src/models/TransformerV4.py
@@ -2,7 +2,9 @@
 import torch
 import torch.nn as nn
 
+from torch.nn import TransformerEncoderLayer
 from input_utils.fft_utils import fft_preprocess
+from models.FusionModules import TransformerFusionBlock
 
 from models.SwinModules import (
     BasicLayer,
@@ -43,90 +45,111 @@ class TransformerV4(nn.Module):
         self.num_segments = args.dataset_config["num_segments"]
 
         # Transformer Variables
-        self.embed_dim = self.config["embed_dim"]
-        self.depths = self.config["depths"]
-        self.num_heads = self.config["num_heads"]
         self.window_size = self.config["window_size"]
         self.drop_rate = self.config["dropout_ratio"]
         self.norm_layer = nn.LayerNorm
-        self.num_features = int(self.embed_dim * 2 ** (len(self.depths) - 1))
-        self.norm = self.norm_layer(self.num_features)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
 
-        # Layers
+        # Single sensor
         self.freq_interval_layers = nn.ModuleDict()  # Frequency & Interval layers
-        self.sensor_layers = nn.ModuleDict()  # Modality Layers
-
-        # split image into non-overlapping patches (? linear embedding)
         self.patch_embed = nn.ModuleDict()
         self.mod_patch_embed = nn.ModuleDict()
+        self.mod_in_layers = nn.ModuleDict()
         for loc in self.locations:
             self.freq_interval_layers[loc] = nn.ModuleDict()
             self.patch_embed[loc] = nn.ModuleDict()
+            self.mod_in_layers[loc] = nn.ModuleDict()
+
             for mod in self.modalities:
+                # Decide the spatial size for "image"
                 stride = self.config["in_stride"][mod]
+                spectrum_len = args.dataset_config["loc_mod_spectrum_len"][loc][mod]
+
                 # Patch embedding and Linear embedding (H, W, in_channel) -> (H / p_size, W / p_size, C)
                 self.patch_embed[loc][mod] = PatchEmbed(
-                    img_size=(10, 20),
-                    patch_size=self.config["patch_size"]["freq"][loc][mod],
+                    img_size=(self.num_segments, spectrum_len // stride),
+                    patch_size=self.config["patch_size"]["freq"][mod],
                     in_chans=args.dataset_config["loc_mod_in_freq_channels"][loc][mod] * stride,
-                    embed_dim=self.embed_dim,
+                    embed_dim=self.config["time_freq_out_channels"],
                     norm_layer=self.norm_layer,
                 )
                 patches_resolution = self.patch_embed[loc][mod].patches_resolution
 
-                # 1 layers Transformer Blocks
+                # Swin Transformer Block
                 self.freq_interval_layers[loc][mod] = nn.ModuleList()
-                for i_layer in range(len(self.depths)):
-                    # Swin Transformer Block
+                for i_layer, block_num in enumerate(self.config["time_freq_block_num"]):  # different downsample ratios
+                    down_ratio = 2**i_layer
+                    layer_dim = int(self.config["time_freq_out_channels"] * down_ratio)
                     layer = BasicLayer(
-                        dim=int(self.embed_dim * 2**i_layer),  # C in SWIN
+                        dim=layer_dim,  # C in SWIN
                         input_resolution=(
-                            patches_resolution[0] // (2**i_layer),  # Patch resolution = (H/4, W/4)
-                            patches_resolution[1] // (2**i_layer),
+                            patches_resolution[0] // down_ratio,  # Patch resolution = (H/4, W/4)
+                            patches_resolution[1] // down_ratio,
                         ),
-                        num_heads=self.num_heads[i_layer],
+                        num_heads=self.config["time_freq_head_num"],
                         window_size=self.window_size,
-                        depth=self.depths[i_layer],
+                        depth=block_num,
                         drop=self.drop_rate,
                         norm_layer=self.norm_layer,
                         downsample=PatchMerging
-                        if (i_layer < len(self.depths) - 1)
+                        if (i_layer < len(self.config["time_freq_block_num"]) - 1)
                         else None,  # Patch merging before transformer blocks
                     )
                     self.freq_interval_layers[loc][mod].append(layer)
 
-            self.mod_patch_embed[loc] = PatchEmbed(
-                img_size=(50, 2),
-                patch_size=self.config["patch_size"]["mod"][loc][mod],
-                in_chans=self.embed_dim,
-                embed_dim=self.embed_dim,
-                norm_layer=self.norm_layer,
-            )
-            patches_resolution = self.mod_patch_embed[loc].patches_resolution
-            # Two layers Transformer Blocks
-            self.sensor_layers[loc] = nn.ModuleList()
-            for i_layer in range(len(self.depths)):
-                # Swin Transformer Block
-                layer = BasicLayer(
-                    dim=int(self.embed_dim * 2**i_layer),  # C in SWIN
-                    input_resolution=(
-                        patches_resolution[0] // (2**i_layer),  # Patch resolution = (H/4, W/4)
-                        patches_resolution[1] // (2**i_layer),
-                    ),
-                    num_heads=self.num_heads[i_layer],
-                    window_size=self.window_size,
-                    depth=self.depths[i_layer],
-                    drop=self.drop_rate,
-                    norm_layer=self.norm_layer,
-                    downsample=PatchMerging
-                    if (i_layer < len(self.depths) - 1)
-                    else None,  # Patch merging before transformer blocks
+                # Unify the input channels for each modality
+                self.mod_in_layers[loc][mod] = nn.Linear(
+                    (patches_resolution[0] // down_ratio) * (patches_resolution[1] // down_ratio) * layer_dim,
+                    self.config["mod_out_channels"],
                 )
-                self.sensor_layers[loc].append(layer)
 
+        # Mod fusion, [b, i, c], mod contextual feature extraction + mod fusion
+        self.mod_context_layers = nn.ModuleDict()
+        self.mod_fusion_layer = nn.ModuleDict()
+        for loc in self.locations:
+            """Single mod contextual feature extraction"""
+            module_list = [
+                TransformerEncoderLayer(
+                    d_model=self.config["mod_out_channels"],
+                    nhead=self.config["mod_head_num"],
+                    dim_feedforward=self.config["mod_out_channels"],
+                    dropout=self.config["dropout_ratio"],
+                    batch_first=True,
+                )
+                for _ in range(self.config["mod_block_num"])
+            ]
+            self.mod_context_layers[loc] = nn.Sequential(*module_list)
+
+            """Mod fusion layer for each loc"""
+            self.mod_fusion_layer[loc] = TransformerFusionBlock(
+                self.config["mod_out_channels"],
+                self.config["mod_head_num"],
+                self.config["dropout_ratio"],
+                self.config["dropout_ratio"],
+            )
+
+        # Loc fusion, [b, i, c], loc contextual feature extraction + loc fusion
+        module_list = [
+            TransformerEncoderLayer(
+                d_model=self.config["loc_out_channels"],
+                nhead=self.config["loc_head_num"],
+                dim_feedforward=self.config["loc_out_channels"],
+                dropout=self.config["dropout_ratio"],
+                batch_first=True,
+            )
+            for _ in range(self.config["loc_block_num"])
+        ]
+        self.loc_context_layer = nn.Sequential(*module_list)
+        self.loc_fusion_layer = TransformerFusionBlock(
+            self.config["loc_out_channels"],
+            self.config["loc_head_num"],
+            self.config["dropout_ratio"],
+            self.config["dropout_ratio"],
+        )
+
+        # Classification layer
         self.class_layer = nn.Sequential(
-            nn.Linear(self.embed_dim, self.config["fc_dim"]),
+            nn.Linear(self.config["loc_out_channels"], self.config["fc_dim"]),
             nn.GELU(),
             nn.Linear(self.config["fc_dim"], args.dataset_config["num_classes"]),
             nn.Sigmoid() if args.multi_class else nn.Softmax(dim=1),
@@ -134,6 +157,7 @@ class TransformerV4(nn.Module):
 
     def forward(self, org_time_x, augmenter):
         args = self.args
+
         # Step 0: Move data to target device
         for loc in org_time_x:
             for mod in org_time_x[loc]:
@@ -162,7 +186,7 @@ class TransformerV4(nn.Module):
                 # Forces both audio and seismic to have the same "img" size
                 freq_input = torch.reshape(freq_input, (b, i, s // stride, c * stride))
 
-                # Repermute back to [b, c, i, spectrum] required in PatchEmbed (b, h, w, c)
+                # Repermute back to [b, c, i, spectrum], (b, c, h, w) required in PatchEmbed
                 freq_input = torch.permute(freq_input, [0, 3, 1, 2])
 
                 # Patch Partition and Linear Embedding
@@ -173,40 +197,41 @@ class TransformerV4(nn.Module):
                     freq_interval_output = layer(embeded_input)
                     embeded_input = freq_interval_output
 
-                # Trying to increase depths
-                for layer in self.freq_interval_layers[loc][mod]:
-                    freq_interval_output = layer(embeded_input)
-                    embeded_input = freq_interval_output
+                # Unify the input channels for each modality
+                freq_interval_output = self.mod_in_layers[loc][mod](freq_interval_output.reshape([b, -1]))
+                freq_interval_output = freq_interval_output.reshape(b, 1, -1)
 
                 # Append the result
                 loc_mod_features[loc].append(freq_interval_output)
-            # Stack results from different modalities
+
+            # Stack results from different modalities, [b, 1, s, c]
             loc_mod_features[loc] = torch.stack(loc_mod_features[loc], dim=2)
 
-        # Step 4: Feature extractions on the modality/sensors
+        # Step 4: Loc mod feature extraction, [b, i, location, c]
         loc_features = []
         for loc in loc_mod_features:
-            # Repermute [b, L (H * W), mod, c] -> [b, c, L, mod] for Patch Embedding
-            mod_input = torch.permute(loc_mod_features[loc], [0, 3, 1, 2])
+            """Extract mod feature with peer-feature context"""
+            b, i, mods, c = loc_mod_features[loc].shape
+            loc_mod_input = loc_mod_features[loc].reshape([b * i, mods, c])
+            loc_mod_context_feature = self.mod_context_layers[loc](loc_mod_input)
+            loc_mod_context_feature = loc_mod_context_feature.reshape([b, i, mods, c])
 
-            # Patch Partition and Linear Embedding on L (time & spectrum) and Modality domains -> [B, M, C]
-            embeded_mod_input = self.mod_patch_embed[loc](mod_input)
-
-            # SwinTransformer Blocks -> [B, M, C]
-            for layer in self.sensor_layers[loc]:
-                mod_output = layer(embeded_mod_input)
-                embeded_mod_input = mod_output
-
-            # Normalize and append to result
-            mod_output = self.norm(mod_output)
-            mod_output = self.avgpool(mod_output.transpose(1, 2))
-            loc_features.append(mod_output)
-
-        # Stack results from different location
+            """Mod feature fusion, [b, 1, 1, c]"""
+            loc_feature = self.mod_fusion_layer[loc](loc_mod_context_feature)
+            loc_features.append(loc_feature)
         loc_features = torch.stack(loc_features, dim=2)
 
-        # Classification Layers
-        sample_features = loc_features.squeeze(dim=2)
+        # Step 5: Location-level fusion, [b, 1, l, c]
+        if len(self.locations) > 1:
+            b, i, l, c = loc_features.shape
+            loc_input = loc_features.reshape([b * i, l, c])
+            loc_context_feature = self.loc_context_layer(loc_input)
+            loc_context_feature = loc_context_feature.reshape([b, i, l, c])
+            sample_features = self.loc_fusion_layer(loc_context_feature)
+        else:
+            sample_features = loc_features.squeeze(dim=2)
+
+        # Step 6: Classification Layers
         sample_features = sample_features.flatten(start_dim=1)
         logit = self.class_layer(sample_features)
 
