e9fefed7bed987967ff7b81f2556a8eea4677a7f
Tomoyoshi Kimura <tomoyoshki@gmail.com>
2023-02-13 21:02:40 -0600

    Fix CMC loss update in eval

--- src/models/loss.py
+++ src/models/loss.py
@@ -303,6 +303,7 @@ class NCEAverage(nn.Module):
 
         ab_view = ab.view(batchSize, inputSize, 1)
         out_ab = torch.bmm(weight_l, ab_view)
+
         # sample
         weight_ab = torch.index_select(self.memory_ab, 0, idx.view(-1)).detach()
         weight_ab = weight_ab.view(batchSize, K + 1, inputSize)
@@ -312,37 +313,40 @@ class NCEAverage(nn.Module):
         if self.use_softmax:
             out_ab = torch.div(out_ab, T)
             out_l = torch.div(out_l, T)
-            out_l = out_l.contiguous()
-            out_ab = out_ab.contiguous()
+            if self.training:
+                out_l = out_l.contiguous()
+                out_ab = out_ab.contiguous()
         else:
             out_ab = torch.exp(torch.div(out_ab, T))
             out_l = torch.exp(torch.div(out_l, T))
             # set Z_0 if haven't been set yet,
             # Z_0 is used as a constant approximation of Z, to scale the probs
-            if Z_l < 0:
-                self.params[2] = out_l.mean() * outputSize
-                Z_l = self.params[2].clone().detach().item()
-            if Z_ab < 0:
-                self.params[3] = out_ab.mean() * outputSize
-                Z_ab = self.params[3].clone().detach().item()
+            if self.training:
+                if Z_l < 0:
+                    self.params[2] = out_l.mean() * outputSize
+                    Z_l = self.params[2].clone().detach().item()
+                if Z_ab < 0:
+                    self.params[3] = out_ab.mean() * outputSize
+                    Z_ab = self.params[3].clone().detach().item()
             # compute out_l, out_ab
             out_l = torch.div(out_l, Z_l).contiguous()
             out_ab = torch.div(out_ab, Z_ab).contiguous()
 
         # # update memory
         with torch.no_grad():
-            l_pos = torch.index_select(self.memory_l, 0, y.view(-1))
-            l_pos.mul_(momentum)
-            l_pos.add_(torch.mul(l, 1 - momentum))
-            l_norm = l_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-            updated_l = l_pos.div(l_norm)
-            self.memory_l.index_copy_(0, y, updated_l)
+            if self.training:
+                l_pos = torch.index_select(self.memory_l, 0, y.view(-1))
+                l_pos.mul_(momentum)
+                l_pos.add_(torch.mul(l, 1 - momentum))
+                l_norm = l_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+                updated_l = l_pos.div(l_norm)
+                self.memory_l.index_copy_(0, y, updated_l)
 
-            ab_pos = torch.index_select(self.memory_ab, 0, y.view(-1))
-            ab_pos.mul_(momentum)
-            ab_pos.add_(torch.mul(ab, 1 - momentum))
-            ab_norm = ab_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-            updated_ab = ab_pos.div(ab_norm)
-            self.memory_ab.index_copy_(0, y, updated_ab)
+                ab_pos = torch.index_select(self.memory_ab, 0, y.view(-1))
+                ab_pos.mul_(momentum)
+                ab_pos.add_(torch.mul(ab, 1 - momentum))
+                ab_norm = ab_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+                updated_ab = ab_pos.div(ab_norm)
+                self.memory_ab.index_copy_(0, y, updated_ab)
 
         return out_l, out_ab
