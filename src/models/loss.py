d99b34d4d9f0cda14ea414a47d5a86a45226825d
Tomoyoshi Kimura <tomoyoshki@gmail.com>
2023-04-12 01:30:04 -0500

    Add InfoNCE loss to GMC

--- src/models/loss.py
+++ src/models/loss.py
@@ -739,6 +739,36 @@ class GMCLoss(nn.Module):
         self.criterion = nn.CrossEntropyLoss(reduction="sum")
         self.similarity_f = nn.CosineSimilarity(dim=2)
 
+    def infonce(self, mod_features):
+        batch_representations = [mod_features[mod] for mod in self.modalities]
+        batch_representations.append(mod_features["joint"])
+
+        batch_size = batch_representations[0].shape[0]
+        joint_mod_loss_sum = 0
+        for mod in range(len(batch_representations) - 1):
+            # Negative pairs: everything that is not in the current joint-modality pair
+            out_joint_mod = torch.cat([batch_representations[-1], batch_representations[mod]], dim=0)
+            # [2*B, 2*B]
+            sim_matrix_joint_mod = torch.exp(torch.mm(out_joint_mod, out_joint_mod.t().contiguous()) / self.temperature)
+            # Mask for remove diagonal that give trivial similarity, [2*B, 2*B]
+            mask_joint_mod = (
+                torch.ones_like(sim_matrix_joint_mod) - torch.eye(2 * batch_size, device=sim_matrix_joint_mod.device)
+            ).bool()
+            # Remove 2*B diagonals and reshape to [2*B, 2*B-1]
+            sim_matrix_joint_mod = sim_matrix_joint_mod.masked_select(mask_joint_mod).view(2 * batch_size, -1)
+
+            # Positive pairs: cosine loss joint-modality
+            pos_sim_joint_mod = torch.exp(
+                torch.sum(batch_representations[-1] * batch_representations[mod], dim=-1) / self.temperature
+            )
+            # [2*B]
+            pos_sim_joint_mod = torch.cat([pos_sim_joint_mod, pos_sim_joint_mod], dim=0)
+            loss_joint_mod = -torch.log(pos_sim_joint_mod / sim_matrix_joint_mod.sum(dim=-1))
+            joint_mod_loss_sum += loss_joint_mod
+
+        loss = torch.mean(joint_mod_loss_sum)
+        return loss
+
     def infonce_with_joints_as_negatives(self, mod_features):
         """ """
         batch_representations = [mod_features[mod] for mod in self.modalities]
@@ -770,7 +800,7 @@ class GMCLoss(nn.Module):
         return loss
 
     def forward(self, mod_features):
-        return self.infonce_with_joints_as_negatives(mod_features)
+        return self.infonce(mod_features)
 
 
 class MAELoss(nn.Module):
