d4860026a26690750fdd14f68413d79d85adbe3f
Tomoyoshi Kimura <tomoyoshki@gmail.com>
2023-04-22 18:20:42 -0500

    Parse std for finetune result

--- scripts/result_utils.py
+++ scripts/result_utils.py
@@ -16,8 +16,9 @@ def convert_result_to_csv(input_json_file):
     # Extract the required information
     for key, values in data.items():
         dataset, model, framework, task, label_ratio = key.split("-")
-        entry = [framework, values["acc"]["mean"], values["f1"]["mean"]]
-        csv_data[(dataset, model, task)][label_ratio].append(entry)
+        mean_entry = [framework, values["acc"]["mean"], values["f1"]["mean"]]
+        std_entry = [framework, values["acc"]["std"], values["f1"]["std"]]
+        csv_data[(dataset, model, task)][label_ratio].append([mean_entry, std_entry])
 
     # Write the information to CSV files
     for (dataset, model, task), label_ratios_data in csv_data.items():
@@ -33,17 +34,26 @@ def convert_result_to_csv(input_json_file):
             # Write header
             header = ["Framework"]
             for label_ratio in sorted_label_ratios:
-                header.extend([f"ACC_{label_ratio}", f"F1_{label_ratio}"])
+                header.extend([f"ACC_{label_ratio}_mean", f"F1_{label_ratio}_mean"])
+
+            for label_ratio in sorted_label_ratios:
+                header.extend([f"ACC_{label_ratio}_std", f"F1_{label_ratio}_std"])
             csvwriter.writerow(header)
 
             # Write data
             for label_ratio_entries in zip(*[label_ratios_data[label_ratio] for label_ratio in sorted_label_ratios]):
                 row = []
+
+                # write mean row
                 for i, entry in enumerate(label_ratio_entries):
                     if i == 0:
-                        row.extend(entry)
+                        row.extend(entry[0])
                     else:
-                        row.extend(entry[1:])
+                        row.extend(entry[0][1:])
+
+                # write std row
+                for i, entry in enumerate(label_ratio_entries):
+                    row.extend(entry[1][1:])
                 csvwriter.writerow(row)
 
 
